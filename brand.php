9da8b254e516451bdc68f0b11846e9519b31e800
Parv Sharma <parvsharma@Parvs-MacBook-Air.local>
2022-05-29 15:21:28 +0530

    Sixth commit

---
+++ brand.php
@@ -0,0 +1,6 @@
+<?php
+	$domainName = "ConvertJpgToPng.Online";
+	$about_site = "Convert JPG to PNG Online for Free";
+	$email = "webbeastyt@gmail.com";
+	$url = "http://convertjpgtopng.online";
+?>
---
+++ header.php
@@ -0,0 +1,24 @@
+<?php include("analytics.php"); ?>
+
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="./" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="32" height="32"><img src='./img/icon.png' style='width: 32px; margin-right: 5px;'/></svg>
+        <h1 class="fs-4" style='text-weight: normal;'><?php echo $domainName; ?></h1>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="./" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">PNG to JPG</a></li>
+        <li class="nav-item"><a href="./about-us.php" class="nav-link">About Us</a></li>
+        <li class="nav-item"><a href="./contact-us.php" class="nav-link">Contact Us</a></li>
+        <li class="nav-item"><a href="./privacy-policy.php" class="nav-link">Privacy Policy</a></li>
+      </ul>
+    </header>
+</div>
+
+<!-- ADVERTISEMENT HEADER -->
+<div class='row'>
+		<div style='width:728px; height: 90px; margin: 0 auto; background: #F00;'></div>	
+</div>
+<!-- ADVERTISEMENT HEADER -->
